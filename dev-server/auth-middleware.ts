d8aabda290cd7a5fe7944348fafd271615a994d3
Hendrik Liebau <mail@hendrik-liebau.de>
2024-03-15 19:52:59 +0100

    Do not warn when `AWS_HANDLER_VERIFY_HEADER` is undefined

--- dev-server/auth-middleware.ts
+++ dev-server/auth-middleware.ts
@@ -5,8 +5,6 @@ const verifyHeader = process.env.AWS_HANDLER_VERIFY_HEADER;
 export const authMiddleware: MiddlewareHandler = async (context, next) => {
   if (verifyHeader) {
     context.req.raw.headers.set(`X-Origin-Verify`, verifyHeader);
-  } else {
-    console.warn(`process.env.AWS_HANDLER_VERIFY_HEADER is undefined`);
   }
 
   return next();
