d6750d7b947e0c9f680a8dc50e061b3b88dc10e8
Hendrik Liebau <mail@hendrik-liebau.de>
2024-03-19 12:58:03 +0100

    Use first user prompt for tape filename

---
+++ dev-server/openai-recorder.ts
@@ -0,0 +1,58 @@
+import path from 'path';
+import filenamify from 'filenamify';
+import talkbackModule from 'talkback';
+import {z} from 'zod';
+
+export interface OpenAiRecorderOptions {
+  readonly port: number;
+}
+
+const chatCompletionsRequestBody = z.object({
+  messages: z.array(
+    z.object({
+      role: z.union([
+        z.literal(`system`),
+        z.literal(`user`),
+        z.literal(`function`),
+        z.literal(`assistant`),
+      ]),
+      content: z.string(),
+    }),
+  ),
+});
+
+// hacky kind of es module interop
+const talkback = talkbackModule as unknown as typeof talkbackModule.default;
+
+export async function startOpenAiRecorder({
+  port,
+}: OpenAiRecorderOptions): Promise<void> {
+  const talkbackServer = talkback({
+    host: `https://api.openai.com`,
+    record: talkback.Options.RecordMode.NEW,
+    port,
+    path: path.join(import.meta.dirname, `tapes/openai`),
+    summary: false,
+    ignoreHeaders: [`user-agent`],
+    tapeNameGenerator: (tapeNumber, tape) => {
+      const result = chatCompletionsRequestBody.safeParse(
+        JSON.parse(tape.req.body.toString(`utf-8`)),
+      );
+
+      if (result.success) {
+        const {messages} = result.data;
+        const userPrompt = messages.find(({role}) => role === `user`)?.content;
+
+        if (userPrompt) {
+          return `${filenamify(userPrompt)}-${tapeNumber}`;
+        }
+      }
+
+      return `unnamed-${tapeNumber}`;
+    },
+  });
+
+  await talkbackServer.start(() =>
+    console.log(`OpenAI talkback server started`),
+  );
+}
--- dev-server/run.ts
+++ dev-server/run.ts
@@ -1,11 +1,16 @@
-import path from 'path';
 import {serve} from '@hono/node-server';
 import {serveStatic} from '@hono/node-server/serve-static';
 import {Hono} from 'hono';
 import {compress} from 'hono/compress';
-import talkbackModule from 'talkback';
 import {authMiddleware} from './auth-middleware.js';
 import './stub-awslambda.js';
+
+if (process.env.OPENAI_BASE_URL?.startsWith(`http://localhost`)) {
+  const {startOpenAiRecorder} = await import(`./openai-recorder.js`);
+  const {port} = new URL(process.env.OPENAI_BASE_URL);
+
+  await startOpenAiRecorder({port: parseInt(port, 10)});
+}
 
 // @ts-ignore
 const handlerModule = await import(`../dist/handler/index.js`);
@@ -25,21 +30,3 @@ serve({fetch: app.fetch, port: 3000}, ({address, port}) => {
 
   return console.log(`Started dev server at ${serverUrl}`);
 });
-
-if (process.env.OPENAI_BASE_URL?.startsWith(`http://localhost`)) {
-  const {port} = new URL(process.env.OPENAI_BASE_URL);
-
-  // hacky kind of es module interop
-  const talkback = talkbackModule as unknown as typeof talkbackModule.default;
-
-  const talkbackServer = talkback({
-    host: `https://api.openai.com`,
-    record: talkback.Options.RecordMode.NEW,
-    port: parseInt(port, 10),
-    path: path.join(import.meta.dirname, `tapes/openai`),
-    summary: false,
-    ignoreHeaders: [`user-agent`],
-  });
-
-  await talkbackServer.start(() => console.log(`Talkback server started`));
-}
